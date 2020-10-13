bfe3b4ae7b06127778d88b469b883a306aa58006
Yurii Bychenok <bychenok.yurii@pdffiller.team>
2020-10-14 02:05:26 +0300

    * formatted with black formatter * now lametric doesn't show current track when soundbar transmits TV signal * updated requirements.txt dependencies

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import requests
 import time
 import os
 
-# import settings
 
 class LaSo:
     def __init__(self, lametric_ip, lametric_user, lametric_api_key, sonos):
@@ -17,53 +16,57 @@ class LaSo:
     def get_track(self):
         track = self.sonos.get_current_track_info()
         status = self.sonos.get_current_transport_info()
-        return track, status
+        is_playing_tv = self.sonos.is_playing_tv
+        return track, status, is_playing_tv
 
     def send_notification(self):
-        track, status = self.get_track()
+        track, status, is_playing_tv = self.get_track()
 
-        if status['current_transport_state'] == 'PLAYING':
+        if status["current_transport_state"] == "PLAYING" and not is_playing_tv:
 
-            artist = track['artist']
-            title = track['title']
+            artist = track["artist"]
+            title = track["title"]
 
             api_url = "http://%s:8080/api/v2/device/notifications" % (self.lametric_ip)
-            headers = {'Content-Type': 'application/json; charset=utf-8'}
+            headers = {"Content-Type": "application/json; charset=utf-8"}
             basicAuthCredentials = (self.lametric_user, self.lametric_api_key)
-            data = '{"model":{"frames":[{"icon":"19113","text":"%s - %s"}]}}' % (artist, title)
+            data = '{"model":{"frames":[{"icon":"19113","text":"%s - %s"}]}}' % (
+                artist,
+                title,
+            )
             try:
-                response = requests.post(api_url,
-                                         headers=headers,
-                                         auth=basicAuthCredentials,
-                                         data=data.encode('utf-8'),
-                                         timeout=3)
+                response = requests.post(
+                    api_url,
+                    headers=headers,
+                    auth=basicAuthCredentials,
+                    data=data.encode("utf-8"),
+                    timeout=3,
+                )
                 # for debugging purpose
                 print(f"{artist} - {title}")
             except requests.exceptions.RequestException as err:
-                print ("OOps: Something Else", err)
+                print("OOps: Something Else", err)
             except requests.exceptions.HTTPError as errh:
-                print ("Http Error:", errh)
+                print("Http Error:", errh)
             except requests.exceptions.ConnectionError as errc:
-                print ("Error Connecting:", errc)
+                print("Error Connecting:", errc)
             except requests.exceptions.Timeout as errt:
-                print ("Timeout Error:", errt)  
+                print("Timeout Error:", errt)
+
 
 def main():
     # If you wish to show specific speaker song use its IP
     # sonos = soco.SoCo('192.168.1.86')
     sonos = soco.discovery.any_soco()
-    laso = LaSo(os.environ["LAMETRIC_IP"],
-                "dev",
-                os.environ["LAMETRIC_API_KEY"],
-                sonos)
+    laso = LaSo(os.environ["LAMETRIC_IP"], "dev", os.environ["LAMETRIC_API_KEY"], sonos)
     # if envar DELAY isn't set than it equals 60
-    delay = os.getenv('DELAY', 60)
-    # print(delay)
+    delay = os.getenv("DELAY", 60)
 
     while True:
         laso.get_track()
         laso.send_notification()
         time.sleep(int(delay))
+
 
 if __name__ == "__main__":
     main()
